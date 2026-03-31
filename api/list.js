9da377345ffc396280610b1adf92ebc5c51d72ec
levinn68 <spartamods77@gmail.com>
2026-03-31 19:24:11 +0700

    Update list.js

--- api/list.js
+++ api/list.js
@@ -2,6 +2,14 @@ const { fetchDB } = require('../lib/github-db')
 
 module.exports = async (req, res) => {
   try {
+    if (req.method !== 'GET') {
+      return res.status(405).json({
+        ok: false,
+        status: 'method_not_allowed',
+        message: 'Method tidak diizinkan'
+      })
+    }
+
     const adminKey = req.headers['x-admin-key']
     if (adminKey !== process.env.SECURITY_ADMIN_KEY) {
       return res.status(401).json({
